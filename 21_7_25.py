17797ac0f00175d82f6db5cb263f0e39a601981f
gudenaveen80 <gudenaveen80@gmail.com>
2025-07-21 18:00:08 +0530

    Add files via upload

---
+++ 21_7_25.py
@@ -0,0 +1,40 @@
+#21-7-25 python assignment
+'''n1=[10,20,30,40]
+sum=0
+for i in n1:
+    sum+=i
+    print(sum, end= " ")
+
+n2=[1,2,3,4,5,6,7,8,9]
+for i in n2:
+    if(i%2==0):
+        print(i,end=" ")
+n3=int(input("enter list:"))
+
+list3=[1,2,3,4,5,6,7,8,9,10]
+count_even=0
+count_odd=0
+for i in list3:
+    if(i%2==0):
+      count_even+=1
+    else:
+      count_odd+=1
+print(count_even,"are even")
+print(count_odd,"are odd")
+
+n =int(input("enter a number:"))
+total = 0
+for i in str(n):
+    total += int(i)
+print("Sum of digits is:", total)
+
+num1=4567
+product=1
+for i in str(num1):
+    product*=int(i)
+    print(product,end=" ")'''
+
+
+
+
+    
