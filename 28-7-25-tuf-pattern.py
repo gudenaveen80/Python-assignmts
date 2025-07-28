84e2612ffac08a9100a53f6bc8979faaff96489e
gudenaveen80 <gudenaveen80@gmail.com>
2025-07-28 23:10:09 +0530

    Add files via upload

---
+++ 28-7-25-tuf-pattern.py
@@ -0,0 +1,83 @@
+# for i in range(1,num1+1):
+#   for j in range(1,num1+1):
+#     print("*",end='')
+#   print()
+
+# n=5
+# for i in range(1,n+1):
+#   for j in range(1,i+1):
+#     print("*",end='')
+#   print()
+
+# n=5
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+
+# n=5
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(i,end="")
+#     print()
+#5
+# n=int(input("enter a number:"))
+# for i in range(0,n+1):
+#     for j in range(0,n-i):
+#         print("*",end="")
+#     print()
+#6
+# 12345
+# 1234
+# 123
+# 12
+# 1
+# n=6
+# for i in range(1,n+1):
+#     for j in range(1,n-i+1):
+#         print(j,end="")
+#     print()
+#    *
+#   ***
+#   *****
+#  *******
+# *********
+# n =5
+# for i in range(0,n+1):
+#     for j in range(n-i):
+#          print(" ",end="")
+#     for j in range(2 * i-1):
+#         print('*',end="")
+#     print()
+
+# enter a number:6
+# ***********
+#  *********
+#   *******
+#   *****
+#     ***
+#      *
+# n=int(input("enter a number:"))
+# for i in range(n,0,-1):
+#     for j in range(n-i):
+#         print(" ",end="")
+#     for j in range(2 * i-1):
+#         print("*", end="")
+#     print()
+
+# 1 
+# 0 1 
+# 1 0 1 
+# 0 1 0 1 
+# 1 0 1 0 1 
+
+# n=5
+
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         if ((i+j)%2==0):
+#             print("1",end=" ")
+#         else:
+#             print("0",end=" ")
+       
+#     print()
