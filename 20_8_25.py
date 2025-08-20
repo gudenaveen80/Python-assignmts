c70c9b50a4d2b0c286f2d025dfee9269fcb1bb4e
gudenaveen80 <gudenaveen80@gmail.com>
2025-08-20 21:09:14 +0530

    Update 20_8_25.py

--- 20_8_25.py
+++ 20_8_25.py
@@ -1,25 +1,24 @@
 #armstrong numbers in a given range.
 def armstrng_or_not(num1):
-    count=0
-    total_sum=0
-    temp=num1
-    while num1>0:
-        count+=1
-        num1=num1//10
-    num1=temp
-    while num1>0:
-        rem=num1%10
-        total_sum+=rem**count
-        num1=num1//10
-    print(total_sum)
-    if temp==sum:
-        return True
-    return False
-armstrong_numbers=[]
-for num1 in range(1,1000):
+    count = 0
+    total_sum = 0
+    temp = num1
+    while num1 > 0:
+        count += 1
+        num1 = num1 // 10  
+    num1 = temp  
+    while num1 > 0:
+        rem = num1 % 10  
+        total_sum += rem ** count  
+        num1 = num1 // 10 
+    if temp == total_sum:
+        return True 
+    return False 
+armstrong_numbers = []
+for num1 in range(1, 1000):
     if armstrng_or_not(num1):
-        armstrong_numbers.append(num1)
-print(" armstrong numbers between 1 to 1000",armstrong_numbers)
+        armstrong_numbers.append(num1)  
+print("Armstrong numbers between 1 and 1000:", armstrong_numbers)
 
 #prime numbers in a given range
 def Prime_range(a,b):
@@ -37,3 +36,4 @@ def Prime_range(a,b):
             print(i)
     print()
 Prime_range(1,1000)
+
